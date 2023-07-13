d2a31e9caefcf10d0fff9c74ea46c7355cf7cdde
Tom Rake <zzzap1957@gmail.com>
2023-07-13 10:00:22 -0400

    Add Round Zero.

---
+++ main.lisp
@@ -0,0 +1,34 @@
+(defpackage #:test-clog/main
+  (:use
+   #:cl
+   #:clog)
+  (:export
+   #:start))
+
+(in-package #:test-clog/main)
+
+(defun on-new-window (body)
+  (let* ((vid (create-video body :source "https://www.w3schools.com/html/mov_bbb.mp4"))
+         (tmp (create-br body))
+         (vpl (create-button body :content ">"))
+         (vst (create-button body :content "||"))
+         (vlc (create-form-element body :input))
+         (tmp (create-hr body))
+         (aud (create-audio body :source "https://www.w3schools.com/html/horse.ogg"))
+         (tmp (create-br body))
+         (apl (create-button body :content ">"))
+         (ast (create-button body :content "||"))
+         (alc (create-form-element body :input))
+         (tmp (create-hr body)))
+    (declare (ignore tmp))
+    (set-on-click vpl (lambda (obj)(declare (ignore obj))(play-media vid)))
+    (set-on-click apl (lambda (obj)(declare (ignore obj))(play-media aud)))
+    (set-on-click vst (lambda (obj)(declare (ignore obj))(pause-media vid)))
+    (set-on-click ast (lambda (obj)(declare (ignore obj))(pause-media aud)))
+    (set-on-time-update vid (lambda (obj)(declare (ignore obj))(setf (value vlc) (media-position vid))))
+    (set-on-time-update aud (lambda (obj)(declare (ignore obj))(setf (value alc) (media-position aud))))))
+
+(defun start ()
+  "Start turtorial."
+  (initialize 'on-new-window)
+  (open-browser))
---
+++ test-clog.asd
@@ -0,0 +1,11 @@
+(defsystem "test-clog"
+  :description "Test how clog works with deploy"
+  :author "Tom Rake<zzzap1967@gmail.com>"
+  :license "MIT"
+  :class :package-inferred-system  
+  :depends-on  ("clog")
+  :components ((:file "main"))
+  :defsystem-depends-on (:deploy)
+  :build-operation "deploy-op"
+  :build-pathname "test-clog"
+  :entry-point "test-clog/main:start")
